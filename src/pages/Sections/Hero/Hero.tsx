aa51aeef5bc9ace0359e8c10c72fa1dfb0d45055
jhonherikgc <jhonherik006@gmail.com>
2025-08-04 20:57:32 +0100

    git ignore atualizado

--- src/pages/Sections/Hero/Hero.tsx
+++ src/pages/Sections/Hero/Hero.tsx
@@ -1,41 +1,69 @@
-import { styled, Typography } from "@mui/material"
-import Avatar from "../../../assets/images/avatar.png.jpg"
-import { Container, Grid} from "@mui/material";
+import { styled, Typography, Button, Container, Grid } from "@mui/material";
 import DownloadIcon from '@mui/icons-material/Download';
 import EmailIcon from '@mui/icons-material/Email';
+import Avatar from "../../../assets/images/avatar.png.jpg";
 
 const Hero = () => {
-
-    const StyleHero = styled("div")(() => ({
+    
+    const StyledHero = styled("section")(({ theme }) => ({
         backgroundColor: "black",
-        height:"100vh"
-    }))
-    const StyledImg = styled ("img")(() => ({
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    }));
+
+    const StyledAvatar = styled("img")(({ theme }) => ({
         width: "100%",
-        borderRadius: "50%"
-    }))
-  return (
-    <>
-    <StyleHero>
-        <Container>
-<Grid container spacing={2}>
-  <Grid size={4}>
-        <StyledImg src = {Avatar} />
-  </Grid>
-  <Grid size={8}>
-    <Typography color="primary" variant="h3">Jhon Herik Gomes de Castro</Typography>
-    <Typography color="primary" variant="h5">I'm a future Software Enginer</Typography>
+        maxWidth: "300px", 
+        borderRadius: "50%",
+        [theme.breakpoints.down('md')]: {
+            marginBottom: theme.spacing(4), 
+        },
+    }));
 
-    <button>
-      <DownloadIcon/>Donwload CV</button>
-    <button>
-      <EmailIcon/>Contact Me</button>
-  </Grid>
-</Grid>
-        </Container>
-    </StyleHero>
-    </>
-  )
-}
+    return (
+        <StyledHero>
+            <Container maxWidth="lg">
+                <Grid 
+                    container 
+                    spacing={4} 
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    <Grid item xs={12} md={4}>
+                        <StyledAvatar src={Avatar} alt="Jhon Herik's Avatar" />
+                    </Grid>
 
-export default Hero
+                    <Grid item xs={12} md={8}>
+                        <Typography color="primary" variant="h3" component="h1" gutterBottom>
+                            Jhon Herik Gomes de Castro
+                        </Typography>
+                        <Typography color="primary" variant="h5" component="p" gutterBottom>
+                            I'm a future Software Engineer
+                        </Typography>
+
+                        <Button 
+                            variant="contained" 
+                            color="primary" 
+                            startIcon={<DownloadIcon />} 
+                            sx={{ mr: 2, mt: 3 }}
+                        >
+                            Download CV
+                        </Button>
+                        <Button 
+                            variant="outlined" 
+                            color="primary" 
+                            startIcon={<EmailIcon />}
+                            sx={{ mt: 3 }}
+                        >
+                            Contact Me
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Container>
+        </StyledHero>
+    );
+};
+
+export default Hero;
