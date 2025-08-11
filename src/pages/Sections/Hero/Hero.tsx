a69224e12b9cc82794b08d03b174dbe40c139341
jhonherikgc <jhonherik006@gmail.com>
2025-08-11 12:24:17 +0100

    ref + scroll-reveal

--- src/pages/Sections/Hero/Hero.tsx
+++ src/pages/Sections/Hero/Hero.tsx
@@ -6,130 +6,134 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import StyledButton from "../../../components/StyledButton/StyledButton";
 import { AnimatedBackground } from "../../../components/AnimatedBackground/AnimatedBackground";
-
+import { Fade, Slide } from "react-awesome-reveal";
 
 // Styled Components
 const StyledHero = styled("div")(({ theme }) => ({
-  backgroundColor: theme.palette.primary.main,
-  height: "100vh",
-  display: "flex",
-  alignItems: "center",
-  [theme.breakpoints.up("xs")]: {
-    paddingTop: "100px",
-  },
-  [theme.breakpoints.up("md")]: {
-    paddingTop: "0",
-  },
+  backgroundColor: theme.palette.primary.main,
+  height: "100vh",
+  display: "flex",
+  alignItems: "center",
+  [theme.breakpoints.up("xs")]: {
+    paddingTop: "100px",
+  },
+  [theme.breakpoints.up("md")]: {
+    paddingTop: "0",
+  },
 }));
 
 const StyledImg = styled("img")(({ theme }) => ({
-  width: "75%",
-  maxWidth: "400px", // Limita o tamanho da imagem em telas muito grandes
-  borderRadius: "50%",
-  border: `1px solid ${theme.palette.primary.contrastText}`,
-  // ✅ Adicionando um estilo para telas de PC para mover a imagem para a esquerda
-  [theme.breakpoints.up("md")]: {
-    paddingLeft: "-100px",
-    marginLeft: "-100px",
-    width:"100%",
-    marginTop:"-15px"
-  },
+  width: "75%",
+  maxWidth: "400px",
+  borderRadius: "50%",
+  border: `1px solid ${theme.palette.primary.contrastText}`,
+  [theme.breakpoints.up("md")]: {
+    paddingLeft: "-100px",
+    marginLeft: "-100px",
+    width:"100%",
+    marginTop:"-15px"
+  },
 }));
 
 // Main Component
 const Hero = () => {
-  return (
-    <StyledHero>
-      <Container maxWidth="lg">
-        <Grid container spacing={2}>
-          {/* Foto e background*/}
-          <Grid>
-            <Box position="relative">
-              <Box position="absolute" width={"150%"} top={-100} right={0}>
-                <AnimatedBackground />
-              </Box>
-              <Box position="relative" textAlign="center">
-                <StyledImg src={Avatar} alt="Avatar" />
-              </Box>
-            </Box>
-          </Grid>
+  return (
+    <StyledHero>
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          {/* Foto e background*/}
+          <Grid>
+            <Slide direction="left">
+              <Box position="relative">
+                <Box position="absolute" width={"150%"} top={-100} right={0}>
+                  <AnimatedBackground />
+                </Box>
+                <Box position="relative" textAlign="center">
+                  <StyledImg src={Avatar} alt="Avatar" />
+                </Box>
+              </Box>
+            </Slide>
+          </Grid>
 
-          {}
-          <Grid>
-            <Typography
-              color="primary.contrastText"
-              variant="h1"
-              textAlign="center"
-              pb={2}
-              sx={{
-                position: {md:"absolute"},
-                marginTop: {md:"-0,01%"},
-                paddingTop: {md:"10px"},
-                marginLeft: {md:"-1%"}
-              }}
-            >
-              Jhon Herik Gomes de Castro
-            </Typography>
-            <Typography
-              color="primary.contrastText"
-              variant="h2"
-              textAlign="center"
-              sx={{
-                position: {md:"absolute"},
-                marginTop: {md:"5%"},
-                paddingTop: {md:"10px"},
-                marginLeft: {md:"-1%"}
-              }}
-            >
-              I'm a Future Software Engineer
-            </Typography>
+          {/* Texto e Botões */}
+          <Grid>
+            <Fade>
+              <Typography
+                color="primary.contrastText"
+                variant="h1"
+                textAlign="center"
+                pb={2}
+                sx={{
+                  position: {md:"absolute"},
+                  marginTop: {md:"-0,01%"},
+                  paddingTop: {md:"10px"},
+                  marginLeft: {md:"-1%"}
+                }}
+              >
+                Jhon Herik Gomes de Castro
+              </Typography>
+            </Fade>
+            <Fade delay={500}>
+              <Typography
+                color="primary.contrastText"
+                variant="h2"
+                textAlign="center"
+                sx={{
+                  position: {md:"absolute"},
+                  marginTop: {md:"5%"},
+                  paddingTop: {md:"10px"},
+                  marginLeft: {md:"-1%"}
+                }}
+              >
+                I'm a Future Software Engineer
+              </Typography>
+            </Fade>
 
-            {/* Botões */}
-            <Grid
-              container
-              display="flex"
-              justifyContent="center"
-              spacing={3}
-              pt={3}
-              sx={{
-                position: {md:"absolute"},
-                marginTop: {md:"10%"},
-                marginLeft: {md:"0"},
-              }}
-            >
-              <Grid>
-                <StyledButton onClick={() => {}}>
-                  <DownloadIcon />
-                  <Typography>Download CV</Typography>
-                </StyledButton>
-              </Grid>
-
-              <Grid>
-                <StyledButton onClick={() => {}}>
-                  <MailOutlineIcon />
-                  <Typography>Contact me</Typography>
-                </StyledButton>
-              </Grid>
-
-                <Grid>
-                <StyledButton onClick={() => {}}>
-                  <LinkedInIcon />
-                  <Typography>Linkedin</Typography>
-                </StyledButton>
-              </Grid>
-
-                <Grid>
-                <StyledButton onClick={() => {}}>
-                  <GitHubIcon />
-                  <Typography>Github</Typography>
-                </StyledButton>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Container>
-    </StyledHero>
-  );
+            {/* Botões */}
+            <Fade delay={1000}>
+              <Grid
+                container
+                display="flex"
+                justifyContent="center"
+                spacing={3}
+                pt={3}
+                sx={{
+                  position: {md:"absolute"},
+                  marginTop: {md:"10%"},
+                  marginLeft: {md:"0"},
+                }}
+              >
+                <Grid>
+                  <StyledButton onClick={() => {}}>
+                    <DownloadIcon />
+                    <Typography>Download CV</Typography>
+                  </StyledButton>
+                </Grid>
+                <Grid>
+                  <StyledButton onClick={() => {}}>
+                    <MailOutlineIcon />
+                    <Typography>Contact me</Typography>
+                  </StyledButton>
+                </Grid>
+                <Grid>
+                  <StyledButton onClick={() => {}}>
+                    <LinkedInIcon />
+                    <Typography>Linkedin</Typography>
+                  </StyledButton>
+                </Grid>
+                <Grid>
+                  <StyledButton onClick={() => {}}>
+                    <GitHubIcon />
+                    <Typography>Github</Typography>
+                  </StyledButton>
+                </Grid>
+              </Grid>
+            </Fade>
+          </Grid>
+        </Grid>
+      </Container>
+    </StyledHero>
+  );
 };
 
 export default Hero;
