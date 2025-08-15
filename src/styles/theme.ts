bcdcee296a7e35f7cc7d594f651858892a411319
jhonherikgc <jhonherik006@gmail.com>
2025-08-15 11:58:00 +0100

    perf-themes

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+import { createTheme, responsiveFontSizes } from "@mui/material";
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2c2b26',
+      
+    },
+    secondary: {
+      main: '#0277bd',
+    },
+  },
+  typography: {
+    fontFamily:"Helvetica Neue",
+  }
+});
+
+theme = responsiveFontSizes(theme);
+export default theme;
